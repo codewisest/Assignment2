476881f45a06c2c491a1dd7a4ff85ccb1952c72d
Chijioke <wisestcodes@gmail.com>
2023-02-23 18:53:44 -0500

    add descriptive comments for j3

--- Assignment2/Controllers/J3Controller.cs
+++ Assignment2/Controllers/J3Controller.cs
@@ -10,17 +10,25 @@ namespace Assignment2.Controllers
 {
     public class J3Controller : ApiController
     {
+        /// <summary>
+        /// provides instruction for tuning harp based on the input string
+        /// <example>AFB+8HC-4 -> AFB tighten 8, HC loosen 4</example>
+        /// </summary>
+        /// <param name="tuneCode">string for tuning harp</param>
+        /// <returns>List of strings of harp tuning code</returns>
         [HttpGet]
         [Route("api/J2/TuneHarp/{tuneCode}")]
         public List<string> TuneHarp(string tuneCode)
         {
+            // create empty
             List<string> tuneNumbers = new List<string> { };
             List<string> tuneInstruction = new List<string> { };
             List<char> tuneOperatorList = new List<char> { };
 
+            // split input string into array of strings
             string[] result = tuneCode.Split('+', '-');
 
-
+            // get all the plus and minus operators in to the list
             for(int i = 0; i < tuneCode.Length; i++)
             {
                 if (tuneCode[i] == '+' || tuneCode[i] == '-')
@@ -29,20 +37,26 @@ namespace Assignment2.Controllers
                 }
             }
 
+            // extract numbers from each split string and add them to the list
             for(int i = 0; i < result.Length; i++)
             {
                 tuneNumbers.Add(Regex.Match(result[i], @"\d+\.*\d*").Value);    
             }
 
-            List<string> resultOnlyString = new List<string> { };
+            List<string> resultOnlyAlphabet = new List<string> { };
             string tuneOperator = "";
+
 
             for (int j = 1; j < result.Length; j++)
             {
+                // dynamically set value fo tune operator i.e tighten or losen
                 tuneOperator = tuneOperatorList[j - 1] == '+' ? "tighten" : "losen";
 
-                resultOnlyString.Add(Regex.Replace(result[j - 1], @"(?<![a-zA-Z])[^a-zA-Z]|[^a-zA-Z](?![a-zA-Z])", ""));
-                tuneInstruction.Add(resultOnlyString[j-1] + " " + tuneOperator + " " + tuneNumbers[j]);
+                // extract only alphabets from the split strings
+                resultOnlyAlphabet.Add(Regex.Replace(result[j - 1], @"(?<![a-zA-Z])[^a-zA-Z]|[^a-zA-Z](?![a-zA-Z])", ""));
+
+                // add the tune instructions
+                tuneInstruction.Add(resultOnlyAlphabet[j-1] + " " + tuneOperator + " " + tuneNumbers[j]);
             }
             return tuneInstruction;
         }
